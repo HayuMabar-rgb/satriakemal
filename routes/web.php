18e8c2d7b2c08de776a43c6a63f6e87bdc4fcc72
HayuMabar-rgb <pasyakemal15@gmail.com>
2020-01-22 11:48:49 +0700

    sasa

--- routes/web.php
+++ routes/web.php
@@ -45,4 +45,59 @@ Route::get('agus/{Alamat}/{sekolah}/{umur}',function($b ,$c , $d)
      'halo ini Sekolahnya Agus :'.$c."<br>" .
      'halo Umur Agus : '.$d. "<br>";
 });
-?>
+Route::get('pesan/{a?}/{b?}/{c?}', function($makan = null , $minum = null , $harga = null){
+if (isset($makan)) {
+    $makan = "Anda memesan $makan";
+    echo $makan;
+}
+if (isset($minum)) {
+    $minum = "& $minum Dengan Harga $ 25000<br>";
+    echo $minum;
+    echo "->25000 Size Medium<br>";
+    echo "->35000 Size large<br>";
+    echo "->25000 small<br>";
+}
+if (!$makan && !$minum) {
+    return "anda belum memesan sesuatu";
+}
+if ($harga == 25000 ) {
+    echo "Anda memesan Kopi dengan Ukuran medium";
+}
+if ($harga == 35000) {
+    echo "Anda memesan Kopi dengan ukuran large";
+}
+if ($harga < 25000) {
+    echo "anda memesan kopi dengan ukuran Small";
+}
+if ($harga < 0) {
+    echo "harga yang anda masukan salah";
+}
+});
+
+Route::get ('Tni/{Nama?}/{BB?}/{Umur?}' , function ($nama = null , $bb = null , $umur = null , $pangkat){
+    if ($nama == null) {
+        echo "Masukan Data Dengan Benar";
+    }    
+    if ($bb == 76-100) {
+        echo "anda harus menurunkan berat badan anda";
+    }    
+    if ($bb == 65-75) {
+        echo "berat badan anda ideal";
+    }
+    if ($bb == 50-64) {
+        echo "berat badan anda kurang";
+    }
+    if ($bb < 50) {
+        echo "Anda Kurang Nutrisi";
+    }
+    
+    if (isset($nama)) {
+        echo "Nama Anda         :$nama";
+    }
+    if (isset($bb)) {
+        echo "berat badan anda  :$bb";
+    }
+    if (isset($umur)) {
+    echo"umur anda adalah $umur";
+    }
+});
